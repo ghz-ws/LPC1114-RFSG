02af65f967d9f995f1aeeeb6135a7cc5bf2afd06
GHz-WS <52226620+ghz-ws@users.noreply.github.com>
2025-08-03 19:37:34 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,34 +1,37 @@
 #include "mbed.h"
-#include "stdio.h"
 
-BufferedSerial uart0(P1_7, P1_6,115200);  //TX, RX
+BufferedSerial uart0(P1_7, P1_6,9600);
 SPI spi(P0_9, P0_8, P0_6);    //mosi, miso, sclk
 DigitalOut le1(P0_7);
 DigitalOut le2(P0_11);
 DigitalIn ld1(P0_4);
 DigitalIn ld2(P0_5);
-
 DigitalOut sel(P0_0);   //CLK sel
-
 BusOut att0(P1_5,P1_4,P1_2,P1_1,P1_0);
 BusOut att1(P1_8,P1_9,P0_1,P0_2,P0_3);
 
-//reg calc from freq
-const uint8_t buf_size=9;
-void buf_read(uint8_t num); //uart read func.
-char read_buf[buf_size];    //uart read buf
-void buf2val();             //buf to vals change func. return to 'freq' and 'pha' global var 
+//CS control
+void spi_send(uint32_t in);
+
+//PLL control
 const uint8_t fpfd=25;
 const uint16_t nmod=25000;  //fpfd/spacing(0.001MHz)
 uint32_t freq;              //kHz
 int8_t ampl;               //means att state
-
-//spi write
-void spi_send(uint8_t ch,uint32_t in);
-void pll_set(uint8_t ch,uint32_t freq);
+void pllset(uint8_t ch,uint32_t freq,uint8_t att);
 uint8_t k;
 uint32_t data[6]={0,0,0x4E42,0x4B3,0,0x580005};
 char local_buf[2];
+uint8_t locked1,locked2;
+uint8_t att;
+uint8_t buf;
+
+//parser
+char char_read();
+float char2flac();
+void parser();
+float pars[4];
+uint32_t a,b,c,d;
 
 int main(){
     sel=0;      //0=int, 1=ext
@@ -36,63 +39,33 @@ int main(){
     le2=1;
     att0=0;
     att1=0;
-    spi.format(8,0);   //spi mode setting. 2byte(16bit) transfer, mode 0
+    spi.format(8,0);   //spi mode setting. 2byte(16bit) transfer, mode 2
+    thread_sleep_for(100);
+
     while (true){
-        for(k=1;k<=2;++k){
-            buf_read(buf_size);//uart buf read
-            buf2val();
-            pll_set(k,freq);
-            if(ampl>=31)ampl=31;
-            if(ampl<0)ampl=0;
-            if(k==1)att0=~ampl;
-            else att1=~ampl;
+        buf=char_read();
+        if(buf=='S'){
+            parser();
+        }else if(buf=='?'){
+            locked1=ld1;
+            locked2=ld2;
+            printf("%d,%d,%04d,%04d\n\r",locked1,locked2,0,0);
         }
-        //lock detect
-        if(ld1==1)local_buf[0]='1';
-        else local_buf[0]='0';
-        thread_sleep_for(1);
-        if(ld2==1)local_buf[1]='1';
-        else local_buf[1]='0';
-        uart0.write(local_buf,2);
+        a=(uint32_t)pars[0];
+        b=(uint32_t)pars[1];
+        c=(uint32_t)pars[2];
+        d=(uint32_t)pars[3];
+
+        if(a==5){
+            pllset(1,b,c);
+        }else if(a==6){
+            pllset(2,b,c);
+        }
     }
 }
 
-//uart char number read func.
-void buf_read(uint8_t num){
-    char local_buf[1];
-    uint8_t i;
-    for (i=0;i<num;++i){
-        uart0.read(local_buf,1);
-        read_buf[i]=local_buf[0];
-    }
-}
-
-//buf to val change func.
-void buf2val(){
-    uint8_t i,j;
-    uint32_t pow10;
-    freq=0;
-    ampl=0;
-    for(i=0;i<7;++i){
-        pow10=1;
-        for(j=0;j<6-i;++j){
-            pow10=10*pow10;
-        }
-        freq=freq+(read_buf[i]-48)*pow10;
-    }
-    for(i=0;i<2;++i){
-        pow10=1;
-        for(j=0;j<1-i;++j){
-            pow10=10*pow10;
-        }
-        ampl=ampl+(read_buf[i+7]-48)*pow10;
-    }
-}
-
-void spi_send(uint8_t ch,uint32_t in){
+void spi_send(uint32_t in){
     uint8_t buf;
-    if(ch==1)le1=0;
-    else le2=0;
     buf=(in>>24)&0xff;
     spi.write(buf);
     buf=(in>>16)&0xff;
@@ -101,13 +74,10 @@ void spi_send(uint8_t ch,uint32_t in){
     spi.write(buf);
     buf=(in>>0)&0xff;
     spi.write(buf);
-    if(ch==1)le1=1;
-    else le2=1;
 }
 
-void pll_set(uint8_t ch,uint32_t freq){
-    uint8_t i,j;
-    uint8_t ndiv;
+void pllset(uint8_t ch,uint32_t freq, uint8_t att){
+    uint8_t i,j,ndiv,att_buf;
     uint16_t nint,nmodulus,nfrac_i,r,a,b;
     float freq_f,temp,nfrac;
     if(freq>4400000)freq=4400000;
@@ -155,9 +125,50 @@ void pll_set(uint8_t ch,uint32_t freq){
     
     //spi send
     for(i=0;i<5;++i){
-        spi_send(ch,data[5-i]);
+        if(ch==1)le1=0;
+        else le2=0;
+        spi_send(data[5-i]);
         thread_sleep_for(3);
+        if(ch==1)le1=1;
+        else le2=1;
     }
     thread_sleep_for(20);
-    spi_send(ch,data[0]);
+    if(ch==1)le1=0;
+    else le2=0;
+    spi_send(data[0]);
+    if(ch==1)le1=1;
+    else le2=1;
+
+    //att
+    if(ch==1)att0=~att;
+    else att1=~att;
+}
+
+char char_read(){
+    char local_buf[1];          //local buffer
+    uart0.read(local_buf,1);    //1-char read
+    return local_buf[0];        //return 1-char
+}
+float char2flac(){
+    char temp[1],local_buf[20];          //local buffer
+    uint8_t i;
+    for(i=0;i<sizeof(local_buf);++i) local_buf[i]='\0'; //init local buf
+    i=0;
+    while(true){
+        temp[0]=char_read();
+        if(temp[0]==',') break; //',' is delimiter
+        local_buf[i]=temp[0];
+        ++i;
+    }
+    return atof(local_buf);
+}
+void parser(){
+    uint8_t i=0;
+    pars[i]=char2flac();
+    ++i;
+    pars[i]=char2flac();
+    ++i;
+    pars[i]=char2flac();
+    ++i;
+    pars[i]=char2flac();
 }
